2e2fcf6da882a52806bb3d369ceff4b9abdc59d7
ko-noguchi <kt.noguchi@gmail.com>
2018-01-21 18:10:22 +0900

    Add Flow

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
+// @flow
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.render(
     <App />,
+    // $FlowFixMe
     document.getElementById('root')
 )
